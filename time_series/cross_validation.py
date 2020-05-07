2c7ef0a2564d0d25aa22fd32989237b6945589f1
Pereira Tiago (extern) (U808475) <tiago.pereira@mobi.ch>
2020-05-07 22:00:52 +0100

    Updated cross_validation in time_series.

--- time_series/cross_validation.py
+++ time_series/cross_validation.py
@@ -56,7 +56,7 @@ class TimeSeriesSplit:
 
         max_test_size = np.round(1 - min_train_size - gap, 2)
         if max_test_size < 0:
-            raise ValueError(f'min_train_size ({min_train_size}) + gap ({gap}) > 1 doesn\'t allow for '
+            raise ValueError(f'min_train_size ({min_train_size}) + gap ({gap}) >= 1 doesn\'t allow for '
                              f'test_size > 0. Try reducing one of the sizes.')
 
         if max_test_size < test_size:
@@ -65,32 +65,33 @@ class TimeSeriesSplit:
             test_size = max_test_size
 
         if period is None:
-            period = np.round((1 - (min_train_size + gap + test_size)) / (n_splits - 1), 3) if n_splits > 1 \
-                else test_size
+            period = 0 if n_splits == 1 else min((np.round((1 - (min_train_size + gap + test_size)) / (n_splits - 1),
+                                                           3), test_size))
 
-        if not allow_overlap and (period < test_size):
-            raise ValueError(f'Current period ({period}) < test_size ({test_size}). '
-                             f'This will lead to overlap in consecutive validation sets, '
-                             f'but allow_overlap is set to False. Either change input ratios or allow_overlap.')
+        if n_splits > 1:
+            if (not allow_overlap) and (period < test_size):
+                raise ValueError(f'Current period ({period}) < test_size ({test_size}). '
+                                 f'This will lead to overlap in consecutive validation sets, '
+                                 f'but allow_overlap is set to False. Either change input ratios or allow_overlap.')
 
-        max_n_splits = int(np.round((1 - (min_train_size + gap + test_size)) / period + 1, 3))
-        if max_n_splits < n_splits:
-            warnings.warn(f'Maximum number of splits possible with current ratios is {max_n_splits}. '
-                          f'Output will only consider {max_n_splits} folds.', stacklevel=2)
-            n_splits = max_n_splits
+            max_n_splits = int(np.round((1 - (min_train_size + gap + test_size)) / period + 1, 3))
+            if max_n_splits < n_splits:
+                warnings.warn(f'Maximum number of splits possible with current ratios is {max_n_splits}. '
+                              f'Output will only consider {max_n_splits} folds.', stacklevel=2)
+                n_splits = max_n_splits
 
         return n_splits, min_train_size, test_size, gap, period
 
     def _get_ratios(self, length):
         min_samples = np.array((self.min_train_size, self.test_size, self.period)) * length
-        _check = min_samples < 1
-        if any(_check):
-            warnings.warn(f'Some ratio(s) requested are smaller than a single sample ({min_samples[_check]}).'
+        _check = min_samples if self.n_splits > 1 else min_samples[:2]
+        if any(_check < 1):
+            warnings.warn(f'Some ratio(s) requested are smaller than a single sample ({_check[_check < 1]}).'
                           f'Ratio(s) will be adjusted to represent a single sample.', stacklevel=2)
-        min_samples = np.clip(min_samples, a_min=1, a_max=None)
+        min_samples[:len(_check)] = np.clip(_check, a_min=1, a_max=None)
 
         min_samples = min_samples.astype(int)
-        if min_samples[2] < min_samples[1]:
+        if (min_samples[2] < min_samples[1]) and (self.n_splits > 1):
             warnings.warn('Period to add at each iteration to trainset is smaller than test_size. This will cause '
                           'overlap between testsets on consecutive folds.', stacklevel=2)
 
@@ -180,19 +181,44 @@ class GroupTimeSeriesSplit(TimeSeriesSplit):
                                                    expanding=expanding)
         self.dependent_feature = dependent_feature
 
-    def split(self, X: DataFrame, **kwargs) -> Tuple[Iterable, Iterable]:
-        """
-        Method to perform the split
+    def split(self, X: DataFrame, check_percentage: bool = True, **kwargs) -> Tuple[Iterable, Iterable]:
+        """ Method to perform the split. Splits the data provided according to the ratios used in class initialization.
+        If a dependent_feature was passed guarantees that the split is on the sorted data by the dependent feature and
+        that no same value of the dependent feature appear on both sides of the split (adjusting the initial ratios)
 
         :param DataFrame X: DataFrame to be splitted
+        :param bool check_percentage: boolean to control whether corrected percentages should be checked and displayed
         :param kwargs: not used. just for sklearn compatibility
         :return: array of indices, array of indices
         """
-        block_values = list(range(len(X))) if self.dependent_feature is None else X[self.dependent_feature].values
-        dep_feat = np.unique(block_values)
-        for x_idx, y_idx in super(GroupTimeSeriesSplit, self).split(dep_feat):
-            if self.dependent_feature is None:
-                yield x_idx, y_idx
-            else:
-                yield np.where(np.isin(block_values, dep_feat[x_idx]))[0], \
-                      np.where(np.isin(block_values, dep_feat[y_idx]))[0]
+        if self.dependent_feature is None:
+            yield super(GroupTimeSeriesSplit, self).split(X, **kwargs)
+        else:
+            # for each split find if there are instances of the dependent_feature on both sides of the split.
+            # If positive remove them from right side and add them to left.
+            # Recompute percentages and if difference to original percentage is > 0.05 present a warning.
+            for train, test in super(GroupTimeSeriesSplit, self).split(X, **kwargs):
+                sorted_ = X[self.dependent_feature].reset_index(drop=True).sort_values()
+                to_shift = np.where(sorted_.values[test] == sorted_.values[train].max())[0]
+
+                if len(to_shift) > 0:
+                    idx = sorted_.index.values
+                    mask = np.ones(test.shape[0], dtype=bool)
+                    mask[to_shift] = False
+                    train, test = np.append(idx[train], idx[test][~mask]), idx[test][mask]
+                    if check_percentage:
+                        self._check_percentage(len(train), len(test), len(sorted_))
+
+                yield train, test
+
+    def _check_percentage(self, train_size, test_size, initial_size):
+        cur_per = train_size/initial_size
+        if cur_per < self.min_train_size:
+            raise ValueError('Due to constraints applied the resulting split originates a train set smaller than the '
+                             '"min_train_size" specified.')
+
+        diff = self.train_size - cur_per if self.expanding else abs(self.train_size - cur_per)
+        if diff > 0.05:
+            warnings.warn(f'Due to constraints applied, returning split proportion is {round(cur_per, 2)}/'
+                          f'{round(test_size/initial_size, 2)} instead of {self.train_size}, {self.test_size}.',
+                          stacklevel=2)
