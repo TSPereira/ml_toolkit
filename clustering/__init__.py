de4c2a15775a532c8ceb548540519d49445cb078
TiagoPereira <Asph".00_github>
2020-12-22 14:18:50 +0000

    clean up

--- clustering/__init__.py
+++
@@ -1,3 +0,0 @@
-from .clustering_algorithms import Cluster
-from .clustering_estimators import Riddle, AsankaPerera, PhamDimovNguyen, GapStatistic, WeightedEstimator
-from .plot_functions import plot_clusters, plot_metrics
