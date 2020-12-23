d4709642a632bbb64a07f88333e1b65891264eb1
TiagoPereira <Asph".00_github>
2020-12-23 11:06:38 +0000

    updated postgres test

--- tests/db_interaction/postgres.py
+++ tests/db_interaction/postgres.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     df = pd.DataFrame({'A': [1, 2, 3], 'B': ['a', 'b', 'c'], 'C': [1, 2, None]})
     df2 = pd.DataFrame({'a': ['1', 2, 3, 4], 'B': ['z', 'b', 'e', 'd'], 'D': [1, 2, None, 3]})
 
-    db = PostgreSQLManager('postgres', 'sia', name='postgres', host='localhost')
+    db = PostgreSQLManager(user='user', password='password', name='postgres', host='localhost')
     db.drop_table(['a', 'b'])
     db.upload_table('a', df)
     print(db.read_table('a'))
